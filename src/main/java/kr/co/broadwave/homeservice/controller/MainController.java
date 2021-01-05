0fc2f5e3e209788adc0309f04ed80d181b877aa3
Minkyu3 <gkhtls2006@broadwave.co.kr>
2021-01-05 13:54:22 +0900

    유저컨트롤러

---
+++ src/main/java/kr/co/broadwave/homeservice/controller/MainController.java
@@ -0,0 +1,41 @@
+package kr.co.broadwave.homeservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 민규
+ * Date : 2021-01-05
+ * Time :
+ * Remark : 관리자용 컨트롤러
+ */
+@Slf4j
+@Controller
+public class MainController {
+
+    // 대시보드 메인화면(사용자용)
+    @RequestMapping("/")
+    public String main() {
+        return "index";
+    }
+
+    // 사용자 - 온도페이지
+    @RequestMapping("/usertemp")
+    public String usertemp() {
+        return "userstate/usertemp";
+    }
+
+    // 사용자 - 습도페이지
+    @RequestMapping("/userhumidity")
+    public String userhumidity() {
+        return "userstate/userhumidity";
+    }
+
+    // 사용자 - 공기질페이지
+    @RequestMapping("/userair")
+    public String userair() {
+        return "userstate/userair";
+    }
+
+}
