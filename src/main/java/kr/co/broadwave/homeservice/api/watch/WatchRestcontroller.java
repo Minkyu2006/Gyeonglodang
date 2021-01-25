5255e67b205da510f4e2aeefb227503275855d41
CIS-Mac_BW <kr.chis@gmail.com>
2021-01-25 13:31:34 +0900

    API서버 /api/v1/watch Get추가(테스트,데이터확인용)

--- src/main/java/kr/co/broadwave/homeservice/api/watch/WatchRestcontroller.java
+++ src/main/java/kr/co/broadwave/homeservice/api/watch/WatchRestcontroller.java
@@ -6,11 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -47,6 +45,10 @@ public class WatchRestcontroller {
         }
         watchService.save(watchMapper);
         return ResponseEntity.ok(res.success());
+    }
+    @GetMapping
+    public List<Watch> watchGetAll(){
+        return watchService.getAll();
     }
 
 
