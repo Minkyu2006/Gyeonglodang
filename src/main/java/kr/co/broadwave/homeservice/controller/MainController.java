d31bdcd47f0fc4e2f7027a7330596494eb39f72b
Minkyu3 <gkhtls2006@broadwave.co.kr>
2021-02-04 13:58:31 +0900

    조명추가

--- src/main/java/kr/co/broadwave/homeservice/controller/MainController.java
+++ src/main/java/kr/co/broadwave/homeservice/controller/MainController.java
@@ -1,8 +1,16 @@
 package kr.co.broadwave.homeservice.controller;
 
+import kr.co.broadwave.homeservice.admin.Account;
+import kr.co.broadwave.homeservice.admin.AccountService;
+import kr.co.broadwave.homeservice.common.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 /**
  * @author 민규
@@ -13,11 +21,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 public class MainController {
+    private final AccountService accountService;
+
+    @Autowired
+    public MainController(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     // 스마트경로당 시작 빈페이지
     @RequestMapping("/")
     public String start() {
         return "start";
+    }
+
+    // 시스템환경설정
+    @RequestMapping("/system")
+    public String system(){
+        return "adminstate/system";
+    }
+
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request){
+        String referrer = request.getHeader("Referer");
+        request.getSession().setAttribute("prevPage", referrer);
+        return "login";
+    }
+
+    @RequestMapping("/loginsuccess")
+    public String loginsuccess(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        String login_ip = CommonUtils.getIp(request);
+        Optional<Account> optionalAccount = accountService.findByUserid(request.getUserPrincipal().getName());
+        if (optionalAccount.isPresent()) {
+            log.info("============Login Success============");
+            log.info(" 로그인 IP " + login_ip);
+            log.info(" session userid " + session.getAttribute("userid"));
+            log.info(" session role " + session.getAttribute("role"));
+            log.info("=====================================");
+        }
+        return "index-admin";
     }
 
     // 대시보드 메인화면(사용자용)
