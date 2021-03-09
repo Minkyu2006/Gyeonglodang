ee2d2c5a5805ea97fa3141663eba47d40f68477d
CIS-Mac_BW <kr.chis@gmail.com>
2021-03-09 11:52:03 +0900

    csrf 토큰 추가

--- src/main/java/kr/co/broadwave/homeservice/configs/WebSecurityConfig.java
+++ src/main/java/kr/co/broadwave/homeservice/configs/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * @author Minkyu
@@ -32,15 +33,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-            .antMatchers("/admindashboard/**").hasAnyRole("ADMIN")
-            .anyRequest().permitAll()
-            .and()
-            .formLogin()
-                .loginPage("/login")
-                .successHandler(successHandler())
-                .failureHandler(failureHandler())
-                .permitAll()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admindashboard/**").hasAnyRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .successHandler(successHandler())
+                    .failureHandler(failureHandler())
+                    .permitAll()
+
         ;
     }
 
